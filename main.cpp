8fb7209bbd4bd7aa9396576429f636d043d47bf1
Drishya Karki <videosforlife1727@gmail.com>
2025-07-09 10:24:00 +0545

    Dont merge

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 // SO, we are starting this project on 7/8/2025
 #include <iostream>
-// #include "Player.h"
-#include "Goblin.h"
-
+#include "Player.h"
+#include "Goblin.h" 
+using namespace std;
 int main(){
 
-    // Player P1;
+    Player P1;
     
     Goblin G1;
 
-    // int player__health = P1.gethealth();
+    int player__health = P1.gethealth();
+    int goblin__health = G1.gethealth();
 
-    // std::cout << player__health;
+    cout << player__health;
+    cout << goblin__health;
 }
